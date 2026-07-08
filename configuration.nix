435ddc32203def747c80a4cef608d201f8d62750
steve winston <stevewinston222@gmail.com>
2026-07-08 12:00:28 +0530

    fix(pkg nvidia driver): change from beta to latest
    Beta is just nvidia's beta branch and need not be at a later version. They could decide not to release betas so switch to more reliable latest branch

--- configuration.nix
+++ configuration.nix
@@ -554,7 +554,7 @@ systemd.settings = {
      nvidiaSettings = true;
  
      # Optionally, you may need to select the appropriate driver version for your specific GPU.
-     package = config.boot.kernelPackages.nvidiaPackages.beta;
+     package = config.boot.kernelPackages.nvidiaPackages.latest;
    };
    hardware.nvidia.prime = {
    		offload.enable = true;
