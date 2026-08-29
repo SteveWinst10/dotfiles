bb9a57db4df0319a7e729788e34fdbf7ffcf3915
steve winston <stevewinston222@gmail.com>
2026-08-29 10:25:49 +0530

    chore(pkg): add kde connect and cisco packet tracer 9.0.0
    The source for cisco-packet-tracer older versions not available on official websites so check other archives.

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,6 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs,lib,inputs, ... }:
-
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -239,11 +238,7 @@
   	 sniffnet
   	 i2p
   	 i2pd
-  	 (cisco-packet-tracer_9.overrideAttrs (oldAttrs: {
-  	   version = "9.0.1";
-  	   src = /home/steve/Downloads/CiscoPacketTracer_901_Ubuntu_64bit.deb;
-  	 }))
-
+	 cisco-packet-tracer_9
   	 kubernetes
   	 
 	 foundry
@@ -264,7 +259,8 @@
 	 python313Packages.flask
 
 	 localsend
-
+	 kdePackages.kdeconnect-kde
+	 
 	 nps
 	 nh
 	 comma
@@ -601,15 +597,6 @@ systemd.settings = {
      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
      # Enable this if you have graphical corruption issues or application crashes after waking
      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-     # of just the bare essentials.
-     powerManagement.enable = true;
- 
-     # Fine-grained power management. Turns off GPU when not in use.
-     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-     powerManagement.finegrained = true;
- 
-     # Use the NVidia open source kernel module (not to be confused with the
-     # independent third-party "nouveau" open source driver).
      # Support is limited to the Turing and later architectures. Full list of 
      # supported GPUs is at: 
      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
@@ -631,7 +618,7 @@ systemd.settings = {
    		nvidiaBusId = "PCI:100:0:0";
                    # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
    	};
-   /*	boot.extraModprobeConfig = ''
+  /* boot.extraModprobeConfig = ''
    	    blacklist nouveau
    	    options nouveau modeset=0
    	  '';
@@ -646,6 +633,6 @@ systemd.settings = {
    	    # Remove NVIDIA VGA/3D controller devices
    	    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
    	  '';
-   	  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
-*/	
+   	  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];	
+  */
 }
